7a32d76b3edaa4d68f304892d2ad60ee06dfb6eb
lakomydavid <lakomydavid@seznam.cz>
2022-02-04 13:54:18 +0100

    2nd commit

--- src/com/engeto/task1b/Main.java
+++ src/com/engeto/task1b/Main.java
@@ -23,33 +23,41 @@ public class Main {
     }
 
     public static void ukol5() {
-        double cena = 0;
+        BigDecimal cena = BigDecimal.valueOf(0);
         for (int i = 0; i < 10; i++) {
-            cena += 0.1; //
+            cena = cena.add(BigDecimal.valueOf(0.1));
         }
         System.out.println(cena);
-        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
-    }
-//
-//    public static void ukol6() {
-//        BigDecimal cena = 0;
-//        cena = cena.add(BigDecimal.valueOf(0.1));
+        /* Má vypsat "1.0". Ale vypíše "0.9999" !!!
+        //    public static void ukol5PresnePricitani() {
+//        double cena = 0;
+//        for (int i = 0; i < 10; i++) {
+//            cena += 0.1;
+//        }
 //        System.out.println(cena);
-//    }
-//
-//    public static void ukol7() {
-//        int vykonMotoru = "120";
-//        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
-//    }
-//
-//    public static void ukol8() {
-//        int velikostKosile = 37;
-//        System.out.println("Velikost košile je: ", velikostKosile, ".");
-//    }
-//
-//    public void ukol9() {
-//        System.out.println("Metoda *skoro* bez chybičky!");
-//    }
+//        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+         */
+    }
+
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.valueOf(0); //podobne jako promenna nahore
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    public static void ukol7() {
+        int vykonMotoru = 206;
+        System.out.println("Výkon Motoru je: "+vykonMotoru+" kW."); //syntaxe
+    }
+
+    public static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: "+velikostKosile+"."); //syntaxe
+    }
+
+    public static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!"); //nebyla definovana metoda na static
+    }
 
     public static void main(String[] args) {
         System.out.println("Oprav chyby a spusť všechny metody!");
@@ -59,9 +67,9 @@ public class Main {
         ukol3();
         ukol4();
         ukol5();
-//        ukol6();
-//        ukol7();
-//        ukol8();
-//        ukol9();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
     }
 }
